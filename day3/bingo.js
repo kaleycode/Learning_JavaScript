b0b3957cb403ddb5390bfd3e0f8e6231a86ec533
kaleycode <readingkale@gmail.com>
2024-09-06 10:10:28 -0700

    bingo.js

--- day3/bingo.js
+++ day3/bingo.js
@@ -1,8 +1,95 @@
-const table = document.querySelector("#tblBingo")
-const letter = document.querySelectorAll(".letters-bingo")
+//still trying to figure this out:
+
 let randAnswer;
 
 document.getElementById("new_round").onclick = function(){
     randAnswer = Math.floor(Math.random() * 100 + 1);
-    document.getElementById("btn").innerHTML = randAnswer;
+    document.getElementById("btn").innerHTML  = randAnswer;
+    if (randAnswer = 55)
+        document.getElementById("_55").textContent = `❌`;
+
+    else if (randAnswer = 99)
+        document.getElementById("_99").textContent = `❌`;
+
+    else if (randAnswer = 7)
+        document.getElementById("_7").textContent = `❌`;
+
+    else if (randAnswer = 4)
+        document.getElementById("_4").textContent = `❌`;
+
+    else if (randAnswer = 8)
+        document.getElementById("_8").textContent = `❌`;
+
+    else if (randAnswer = 9)
+        document.getElementById("_9").textContent = `❌`;
+
+    else if (randAnswer = 33)
+        document.getElementById("_33").textContent = `❌`;
+
+    else if (randAnswer = 21)
+        document.getElementById("_21").textContent = `❌`;
+
+    else if (randAnswer = 20)
+        document.getElementById("_20").textContent = `❌`;
+
+    else if (randAnswer = 15)
+        document.getElementById("_15").textContent = `❌`;
+
+    else if (randAnswer = 3)
+        document.getElementById("3").textContent = `❌`;
+
+    else if (randAnswer = 66)
+        document.getElementById("_66").textContent = `❌`;
+
+    else if (randAnswer = 93)
+        document.getElementById("_93").textContent = `❌`;
+
+    else if (randAnswer = 82)
+        document.getElementById("_82").textContent = `❌`;
+
+    else if (randAnswer = 84)
+        document.getElementById("_84").textContent = `❌`;
+
+    else if (randAnswer = 2)
+        document.getElementById("_2").textContent = `❌`;
+
+    else if (randAnswer = 38)
+        document.getElementById("_38").textContent = `❌`;
+
+    else if (randAnswer = 5)
+        document.getElementById("_5").textContent = `❌`;
+
+    else if (randAnswer = 73)
+        document.getElementById("_73").textContent = `❌`;
+
+    else if (randAnswer = 67)
+        document.getElementById("_67").textContent = `❌`;
+
+    else if (randAnswer = 52)
+        document.getElementById("_52").textContent = `❌`;
+
+    else if (randAnswer = 34)
+        document.getElementById("_34").textContent = `❌`;
+
+    else if (randAnswer = 31)
+        document.getElementById("_31").textContent = `❌`;
+
+    else if (randAnswer = 10)
+        document.getElementById("_10").textContent = `❌`;
+
+    else if (randAnswer = 50)
+        document.getElementById("_50").textContent = `❌`;
+
 }
+
+
+
+
+//const table = document.querySelector("#tblBingo")
+//const letter = document.querySelectorAll(".letters-bingo")
+//let randAnswer;
+
+//document.getElementById("new_round").onclick = function(){
+//    randAnswer = Math.floor(Math.random() * 100 + 1);
+//    document.getElementById("btn").innerHTML = randAnswer;
+//}
